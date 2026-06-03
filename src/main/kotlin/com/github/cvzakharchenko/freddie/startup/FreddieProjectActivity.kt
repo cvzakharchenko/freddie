498d9e7a3eedd95409e86599a75e96bf3faf7d26
Kanstantsin Zakharchanka <k_zakharchanka@wargaming.net>
2026-06-03 09:16:29 +0200

    Run Freddie startup on EDT with read access

--- src/main/kotlin/com/github/cvzakharchenko/freddie/startup/FreddieProjectActivity.kt
+++ src/main/kotlin/com/github/cvzakharchenko/freddie/startup/FreddieProjectActivity.kt
@@ -3,13 +3,23 @@ package com.github.cvzakharchenko.freddie.startup
 import com.github.cvzakharchenko.freddie.actions.MercuryInlineCompletionActionBridgeService
 import com.github.cvzakharchenko.freddie.controller.MercuryNextEditController
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.WriteIntentReadAction
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 
 class FreddieProjectActivity : ProjectActivity {
     override suspend fun execute(project: Project) {
-        ApplicationManager.getApplication().service<MercuryInlineCompletionActionBridgeService>().ensureInstalled()
-        project.service<MercuryNextEditController>().start()
+        ApplicationManager.getApplication().invokeLater(
+            {
+                if (project.isDisposed) return@invokeLater
+                WriteIntentReadAction.run {
+                    ApplicationManager.getApplication().service<MercuryInlineCompletionActionBridgeService>().ensureInstalled()
+                    project.service<MercuryNextEditController>().start()
+                }
+            },
+            ModalityState.nonModal(),
+        )
     }
 }
